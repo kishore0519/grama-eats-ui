13f00e842576336475a8042d38f0c4b299a5462b
kishore0519 <kishorekoppolu1905@gmail.com>
2026-07-17 12:43:37 +0530

    feat: Configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./App.js",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
